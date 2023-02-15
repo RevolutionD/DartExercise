df7b17af4f4086c571671e2cc2fd3bc3b11609a4
Noname Gogeto <biendo347@gmail.com>
2023-02-15 21:13:24 +0700

    Week 2 exercise

--- lib/Week 2/exercise2.dart
+++ lib/Week 2/exercise2.dart
@@ -36,5 +36,58 @@ void ex4PrintName100Times() {
 
 void ex5Sum() {
   print('Input number1:');
-  int num1;
+  int num1 = int.parse(stdin.readLineSync()!);
+  print('Input number2:');
+  int num2 = int.parse(stdin.readLineSync()!);
+  print('Sum of $num1 and $num2 is ${num1 + num2}');
+}
+
+void ex6GenerateMultiplicationTableOf5() {
+  print('Multiplication table of 5');
+  for (var index = 1; index <= 10; index++) {
+    print('5 x $index = ${5 * index}');
+  }
+}
+
+void ex7GenerateMultiplicationTable1to9() {
+  print('Multiplication table 1 to 9');
+  for (var i = 1; i <= 9; i++) {
+    for (var j = 1; j <= 10; j++) {
+      print('$i x $j = ${i * j}');
+    }
+  }
+}
+
+void ex8SimpleCalculator() {
+  print('Enter numbers:');
+  String numbersInput = stdin.readLineSync()!;
+  List<double> numbers = numbersInput.split(' ').map((e) => double.parse(e)).toList();
+  print('Enter operator(+, -, *, /):');
+  String operator = stdin.readLineSync()!;
+  double result = 0;
+
+  switch (operator) {
+    case '+':
+      result = numbers.reduce((value, element) => value + element);
+      break;
+    case '-':
+      result = numbers.reduce((value, element) => value - element);
+      break;
+    case '*':
+      result = numbers.reduce((value, element) => value * element);
+      break;
+    case '/':
+      result = numbers.reduce((value, element) => value / element);
+      break;
+    default:
+      print('Invalid operator');
+      break;
+  }
+}
+
+void ex9PrintNumber() {
+  print('Print number to 100 except 41:');
+  for (var i = 0; i <= 100; i++) {
+    if (i != 41) print(i);
+  }
 }
